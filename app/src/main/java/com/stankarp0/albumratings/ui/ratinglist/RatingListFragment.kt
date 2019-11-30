ef0403879b0e56e2f3f105b6e8525d6494ba0b5d
stankarp0 <stankarp0@gmail.com>
2019-11-30 22:25:28 +0100

    rating list fragment added

---
+++ app/src/main/java/com/stankarp0/albumratings/ui/ratinglist/RatingListFragment.kt
@@ -0,0 +1,63 @@
+package com.stankarp0.albumratings.ui.ratinglist
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import com.stankarp0.albumratings.R
+import com.stankarp0.albumratings.databinding.FragmentAlbumDetailsBinding
+import com.stankarp0.albumratings.databinding.FragmentRatingListBinding
+import com.stankarp0.albumratings.ui.adapters.RatingRecyclerAdapter
+import com.stankarp0.albumratings.ui.albumdetails.AlbumDetailsFragmentArgs
+import com.stankarp0.albumratings.ui.albumdetails.AlbumDetailsFragmentDirections
+import com.stankarp0.albumratings.ui.albumdetails.AlbumDetailsViewModel
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RatingListFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var adapter: RatingRecyclerAdapter
+
+    private val viewModel: RatingListViewModel by lazy {
+        ViewModelProviders.of(this).get(RatingListViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentRatingListBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_rating_list, container, false
+        )
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+        // Giving the binding access to the OverviewViewModel
+        binding.viewModel = viewModel
+
+        // RecyclerView
+        recyclerView = binding.recyclerView
+        linearLayoutManager = LinearLayoutManager(inflater.context)
+        recyclerView.layoutManager = linearLayoutManager
+        adapter = RatingRecyclerAdapter()
+        recyclerView.adapter = adapter
+
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/stankarp0/albumratings/ui/ratinglist/RatingListViewModel.kt
@@ -0,0 +1,47 @@
+package com.stankarp0.albumratings.ui.ratinglist
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.stankarp0.albumratings.services.RatingApi
+import com.stankarp0.albumratings.services.RatingObject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class RatingListViewModel : ViewModel() {
+
+    // The internal MutableLiveData String that stores the most recent response
+    private val _ratingObject = MutableLiveData<RatingObject>()
+
+    // The external immutable LiveData for the response String
+    val ratingObject: LiveData<RatingObject>
+        get() = _ratingObject
+
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    init {
+        updateRatings()
+    }
+
+    private fun updateRatings() {
+        coroutineScope.launch {
+            val randomDeferred = RatingApi.retrofitService.all()
+            try {
+                val result = randomDeferred.await()
+                _ratingObject.value = result
+            } catch (e: Exception) {
+                Log.e("RatingListViewModel","Failure: ${e.message}")
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+}
